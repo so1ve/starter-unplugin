60397cdc3ecea234d8513b9b780acdcfba1034d1
Ray <i@mk1.io>
2023-09-02 17:31:11 +0800

    Update index.ts

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,10 +1,10 @@
 import { createUnplugin } from "unplugin";
-
+import type { UnpluginFactory } from 'unplugin'
 import type { Options } from "./types";
 
-export default createUnplugin<Options | undefined>((_options) => ({
+export const unpluginFactory:UnpluginFactory<Options | undefined>=_options => ({
 	name: "pkg-name",
 	transform(code) {
 		return code;
 	},
-}));
+})
